fab1ff3d90174760ad04f4cd7e0c736bf887cd2b
Shivang Mistry <shivangmistry@Shivangs-MacBook-Pro.local>
2018-12-08 17:12:42 -0500

    Initial Commit

---
+++ PureML2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PureML2
+//
+//  Created by Shivang Mistry on 2018-12-08.
+//  Copyright © 2018 Shivang Mistry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
