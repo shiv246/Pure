298f45e26e0cb3f524ec0a8436bce86abfc9dc9e
Shivang Mistry <shivangmistry@Shivangs-MacBook-Pro.local>
2018-12-09 11:22:56 -0500

    first commit

--- PureML2/ViewController.swift
+++ PureML2/ViewController.swift
@@ -7,14 +7,99 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    let identifierLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(displayP3Red: 30/255, green: 67/255, blue: 99/255, alpha: 255/255)
+        label.textAlignment = .center
+        label.font = UIFont(name: "Futura", size: 25)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+         let topColor = UIColor(displayP3Red: 190/255, green: 235/255, blue: 159/255, alpha: 255/255)
+        let bottomColor = UIColor(displayP3Red: 121/255, green: 189/255, blue: 143/255, alpha: 255/255)
+            
+            let gradientColors: [CGColor] = [topColor.cgColor , bottomColor.cgColor]
+            let gradientLocations: [Float] = [0.0, 1.0]
+                
+                let gradientLayer: CAGradientLayer = CAGradientLayer()
+                gradientLayer.colors = gradientColors
+            gradientLayer.locations = gradientLocations as [NSNumber]
+                
+                gradientLayer.frame = self.view.bounds
+            self.view.layer.insertSublayer(gradientLayer, at: 0)
+                
+            
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+       
+        
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+            
+        
+        
+        
+        //        VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
+        
+        setupIdentifierConfidenceLabel()
     }
-
-
+    
+    fileprivate func setupIdentifierConfidenceLabel() {
+        view.addSubview(identifierLabel)
+        identifierLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32).isActive = true
+        identifierLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        identifierLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        identifierLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        //        print("Camera was able to capture a frame:", Date())
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        
+        guard let model = try? VNCoreMLModel(for: ImageClassifier().model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            
+            //perhaps check the err
+            
+            //            print(finishedReq.results)
+            
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+            DispatchQueue.main.async {
+                self.identifierLabel.text = "\(firstObservation.identifier) \(firstObservation.confidence * 100)"
+            }
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
 }
 
